2062164c22a4e7243f0b4024dbf843aa5856e23c
Viliam <vatvil@gmail.com>
2024-01-25 22:19:43 +0100

    add shop template

--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("<h1>shop</h1>");
+  res.render("shop", { pageTitle: "Shop" });
 });
 
 module.exports = router;
